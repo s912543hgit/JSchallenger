c16a124f7ad2a61ece5d17e33663562fa7494744
wang Angel <>
2022-09-19 08:45:53 +0900

    add js basic

---
+++ 01_JavaScript_Basic/19.js
@@ -0,0 +1,7 @@
+// Find the position of one string in another
+// Write a function that takes a string as argument
+// The string contains the substring 'is'
+// Return the index of 'is'
+function myFunction(a) {
+  return a.indexOf('is');
+}
